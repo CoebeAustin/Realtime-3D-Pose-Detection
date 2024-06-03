5a964177d15b3d37673bea892147657fdc8b3274
Coebe <cvlupac1@up.edu.ph>
2024-06-03 13:51:02 +0800

    working for both arms

--- pose_detector.py
+++ pose_detector.py
@@ -1,6 +1,5 @@
 import cv2
 import mediapipe as mp
-import matplotlib.pyplot as plt
 import os
 import tensorflow as tf
 import numpy as np
@@ -89,13 +88,27 @@ def detectPose(image, pose):
             # Append the landmark into the list.
             landmarks.append((int(landmark.x * width), int(landmark.y * height), (landmark.z * width)))
     else:
-        # If no landmarks are detected, display a message on the image.
-        cv2.putText(output_image, "No bodies detected", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        # If no landmarks are detected, display a centered message on the image.
+        text = "No bodies detected"
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 1
+        thickness = 2
+        color = (0, 0, 255)
+
+        # Get the size of the text box
+        text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
+
+        # Calculate the position for centered text
+        text_x = (width - text_size[0]) // 2
+        text_y = (height + text_size[1]) // 2
+
+        # Put the text on the image
+        cv2.putText(output_image, text, (text_x, text_y), font, font_scale, color, thickness, cv2.LINE_AA)
 
     # Return the output image and the found landmarks.
     return output_image, landmarks
 
-def calculate_angle(a,b,c):
+def calculate_angle(a, b, c):
     a = np.array(a)
     b = np.array(b)
     c = np.array(c)
@@ -110,8 +123,10 @@ def calculate_angle(a,b,c):
 
 # Initialize the webcam.
 cap = cv2.VideoCapture(0)
-counter = 0
-stage  = None
+left_counter = 0
+right_counter = 0
+left_stage = None
+right_stage = None
 
 # Check if the webcam is opened correctly.
 if not cap.isOpened():
@@ -134,19 +149,39 @@ while cap.isOpened():
     output_frame, landmarks = detectPose(frame, pose)
 
     if landmarks:
-        shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value][0], landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value][1]]
-        elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value][0], landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value][1]]
-        wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value][0], landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value][1]]
-        angle = calculate_angle(shoulder, elbow, wrist)
-        cv2.putText(output_frame, str(angle), tuple(elbow), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+        left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value][0], landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value][1]]
+        left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value][0], landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value][1]]
+        left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value][0], landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value][1]]
+        right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value][0], landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value][1]]
+        right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value][0], landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value][1]]
+        right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value][0], landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value][1]]
+        left_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
+        right_angle = calculate_angle(right_shoulder, right_elbow, right_wrist)
+        cv2.putText(output_frame, str(left_angle), tuple(left_elbow), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(output_frame, str(right_angle), tuple(right_elbow), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
-        if angle > 160:
-            stage = "down"
-        if angle < 30 and stage == "down":
-            stage = "up"
-            counter += 1
-            print(counter)
-            
+        if left_angle > 160:
+            left_stage = "down"
+        if left_angle < 30 and left_stage == "down":
+            left_stage = "up"
+            left_counter += 1
+            print(f"Left counter: {left_counter}")
+
+        if right_angle > 160:
+            right_stage = "down"
+        if right_angle < 30 and right_stage == "down":
+            right_stage = "up"
+            right_counter += 1
+            print(f"Right counter: {right_counter}")
+
+    # Display the left counter on the top-left side
+    cv2.putText(output_frame, f'Left Counter: {left_counter}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+
+    # Display the right counter on the top-right side
+    right_text = f'Right Counter: {right_counter}'
+    text_size = cv2.getTextSize(right_text, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
+    cv2.putText(output_frame, right_text, (frame.shape[1] - text_size[0] - 10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+
     # Display the output frame.
     cv2.imshow('Pose Detection', output_frame)
 
